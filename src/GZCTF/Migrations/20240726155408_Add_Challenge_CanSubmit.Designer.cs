4becc08544c206898168866215f98f8469ca1f85
Kengwang <github@kengwang.com.cn>
2024-07-26 23:54:52 +0800

    fix: add Migration of CanSubmit

--- src/GZCTF/Migrations/20240726155408_Add_Challenge_CanSubmit.Designer.cs
+++ src/GZCTF/Migrations/20240726155408_Add_Challenge_CanSubmit.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GZCTF.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GZCTF.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240726155408_Add_Challenge_CanSubmit")]
+    partial class Add_Challenge_CanSubmit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/GZCTF/Migrations/20240726155408_Add_Challenge_CanSubmit.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GZCTF.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Challenge_CanSubmit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CanSubmit",
+                table: "GameChallenges",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CanSubmit",
+                table: "ExerciseChallenges",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CanSubmit",
+                table: "GameChallenges");
+
+            migrationBuilder.DropColumn(
+                name: "CanSubmit",
+                table: "ExerciseChallenges");
+        }
+    }
+}
