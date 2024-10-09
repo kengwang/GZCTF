1579f714927b31fafdab210c1815a2af4adfa386
Lil-Ran <lilran4869@qq.com>
2024-10-09 18:02:20 +0800

    Revert "feat(frontend): 允许绕过页面宽度限制"
    This reverts commit 0601cc71a1df26654b4b4022690bf973039c8c1a.

--- src/GZCTF/ClientApp/src/components/WithWiderScreen.tsx
+++ src/GZCTF/ClientApp/src/components/WithWiderScreen.tsx
@@ -1,5 +1,5 @@
-import { Button, Stack, Text, Title } from '@mantine/core'
-import { useLocalStorage, useViewportSize } from '@mantine/hooks'
+import { Stack, Text, Title } from '@mantine/core'
+import { useViewportSize } from '@mantine/hooks'
 import { FC } from 'react'
 import { useTranslation } from 'react-i18next'
 import IconWiderScreenRequired from '@Components/icon/WiderScreenRequiredIcon'
@@ -15,20 +15,12 @@ const WithWiderScreen: FC<WithWiderScreenProps> = ({ children, minWidth = 1080 }
   const { t } = useTranslation()
 
   const tooSmall = minWidth > 0 && view.width > 0 && view.width < minWidth
-  const [forceDisplay, setForceDisplay] = useLocalStorage({
-    key: 'wider-screen-force-display',
-    defaultValue: 0,
-    getInitialValueInEffect: false,
-  })
 
-  return tooSmall && forceDisplay < Date.now() ? (
-    <Stack className={classes.board}>
+  return tooSmall ? (
+    <Stack gap={0} className={classes.board}>
       <IconWiderScreenRequired />
       <Title order={1}>{t('common.content.wider.title')}</Title>
       <Text fw="bold">{t('common.content.wider.text')}</Text>
-      <Button variant='light' size='xs' onClick={() => setForceDisplay(Date.now() + 10 * 60 * 1000)}>
-        {t('common.content.wider.button')}
-      </Button>
     </Stack>
   ) : (
     <>{children}</>
